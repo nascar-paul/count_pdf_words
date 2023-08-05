c72a848f23637c73934f04fac09af387617163f3
nascar-paul <paul.tyler.jones@gmail.com>
2023-08-05 08:09:39 +0800

    Creates pdf parser using PyPDF2

---
+++ count_pdf_words.py
@@ -0,0 +1,35 @@
+import PyPDF2
+from collections import Counter
+import re
+
+# Function to extract text from PDF
+
+def extract_text_from_pdf(pdf_path):
+	with open(pdf_path, 'rb') as file:
+		reader = PyPDF2.PdfFileReader(file)
+		text = ''
+		for page_num in range(reader.numPages):
+			text += reader.getPage(page_num).extractText()
+		return text
+
+# Function to analyze words and their frequency
+
+def analyze_text(text):
+	#Removing punctuation and onverting to lowercase
+	words = re.findall(r'\w+', text.lower())
+	word_count = Counter(words)
+	return word_count
+
+#Path to the PDF file
+pdf_path = 'path/to/the/pdf/file.pdf'
+
+#Extracting text from PDF
+text = extract_text_from_pdf(pdf_path)
+
+#Analyzing text
+word_count = analyze_text(text)
+
+#Printing to table
+print("Word\Frequency")
+for word, count in word_count.items():
+	print(f"(word)\t(count)")
